8967de905b556ab082011d06bc05b473a52e60ba
Carlos Mattoso <calmattoso@hotmail.com>
2014-04-19 21:17:57 -0300

    Fixed ToString and AppendDigit. Pending implementation of bloom filter to speed up digit lookup even more.

--- src/ex2/number.cpp
+++ src/ex2/number.cpp
@@ -1,33 +1,79 @@
 #include "number.h"
 
+#include <deque>
+#include <iostream>
+
 /* Red black tree of digit and index in number ( assures fast digit detection ) */
+
+Number::Number( const Number& copy , unsigned int newMaxLen )
+{
+  this->tree = copy.tree;
+  this->maxLen = ( ( newMaxLen == 0 ) ? copy.maxLen : newMaxLen ) ;
+}
+
+Number::Number( std::string digit , unsigned int maxLen ) : 
+  tree( Number::CompareDigits )
+{
+  this->maxLen = maxLen ;
+
+  if( !digit.empty() )
+    AppendDigit( digit );
+
+  //SetUpFilter( this->maxLen );
+}
 
 bool Number::CompareDigits (  DigitAtPos lhs , DigitAtPos rhs ) 
 {
   return lhs.first < rhs.first ;
 }
 
-Number::Number( const Number& copy )
+/*
+void Number::SetUpFilter( unsigned int projectedCount ,
+                          double falsePositiveRate )
 {
-  this->tree = copy.tree;
+  bloom_parameters parameters;
+
+  // How many elements roughly do we expect to insert?
+  parameters.projected_element_count = projectedCount ;
+
+  // Maximum tolerable false positive probability? (0,1)
+  parameters.false_positive_probability = falsePositiveRate ; 
+
+  if (!parameters)
+  {
+    std::cout << "Error in filter parameters." << std::endl;
+    exit(1);
+  }
+
+  parameters.compute_optimal_parameters();
+
+  this->filter = bloom_filter( parameters ) ;
 }
+*/
 
-Number::Number( std::string digit ) : 
-  tree( Number::CompareDigits )
+bool Number::AppendDigit( std::string  digit )
 {
-  if( !digit.empty() )
-    AppendDigit( digit );
-}
+  unsigned int length = tree.size() + 1 ;
 
+  if( length > maxLen )
+    return false ;
 
-void Number::AppendDigit( std::string  digit )
-{
-  DigitAtPos last = make_pair( digit , tree.size() + 1 );
+  DigitAtPos last = make_pair( digit , length );
+  tree.insert( last );
+
+  return true;
 }
 
 bool Number::HasDigit( std::string digit )
 {
   DigitAtPos d = make_pair( digit , 0 );
+
+
+  /* Bloom (filter) tells us (digit) is **certainly not** in (number) 
+  if( !filter.contains( digit ) )
+    return false;*/
+
+  /* The digit might be in the number, so look it up in the tree */
   return ( ( tree.find( d ) != tree.end() ) ? true : false );
 }
 
@@ -39,14 +85,23 @@ unsigned int Number::GetLength()
 std::string Number::ToString( )
 {
   std::string output = "";
-  std::set< DigitAtPos , bool(*)( DigitAtPos , DigitAtPos ) >::iterator itr;
 
-  for( itr = tree.begin() ; itr != tree.end() ; ++itr )
+  std::deque<std::string> pieces( tree.size() , "" );
+  std::deque<std::string>::iterator piece;
+
+  std::set< DigitAtPos , bool(*)( DigitAtPos , DigitAtPos ) >::iterator digit;
+
+  /* Gather each digit and store them at its respective index in (pieces) */
+    for( digit = tree.begin() ; digit != tree.end() ; ++digit )
+      pieces[ digit->second - 1 ] = digit->first;
+
+  /* Transform (pieces) into a formatted string */
+  for( piece = pieces.begin(); piece != pieces.end() ; ++piece )
   {
     output
       .append( "[" )
-      .append( itr->first )
-      .append( "]" );
+      .append( *piece )
+      .append( "]");
   }
 
   return output;
--- src/ex2/number.h
+++ src/ex2/number.h
@@ -4,22 +4,41 @@
 #include <string>
 #include <set>
 
+#include "../lib/BloomFilter/bloom_filter.hpp"
+
 typedef std::pair< std::string , int > DigitAtPos ;
 
 class Number {
 private:
+
+/* CLASS MEMBERS */ 
+
+  static const unsigned int NUMBER_MAX_LEN = 10;
   
   static bool CompareDigits (  DigitAtPos , DigitAtPos ) ;
 
+/* INSTANCE ATTRIBUTES */
+
   /* Red black tree of digit and index in number ( assures fast digit detection ) */
+  
   std::set< DigitAtPos , bool(*)( DigitAtPos , DigitAtPos ) > tree ;
+  
+  unsigned long maxLen;
+
+
+  bloom_filter filter ;
+
+/* HELPER */
+
+  void SetUpFilter( unsigned int projectedCount = NUMBER_MAX_LEN ,
+                    double falsePositiveRate = 0.01 ) ;
 
 public:
 
-  Number( const Number& );
-  Number( std::string digit = "" );
+  Number( const Number& , unsigned int newMaxLen = 0 );
+  Number( std::string digit = "" , unsigned int maxLen = NUMBER_MAX_LEN  );
 
-  void AppendDigit( std::string );
+  bool AppendDigit( std::string );
 
   bool HasDigit( std::string );
 
